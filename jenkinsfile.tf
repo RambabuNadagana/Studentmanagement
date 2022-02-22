32194da87bed181a9fa958b430d8dc3f7c42b140
RambabuNadagana <94271381+RambabuNadagana@users.noreply.github.com>
2022-02-22 23:21:40 +0530

    Update jenkinsfile.tf

--- jenkinsfile.tf
+++ jenkinsfile.tf
@@ -16,26 +16,13 @@ resource "aws_db_instance" "default" {
   vpc_security_group_ids = ["sg-0bb5391635b3c304e"]
 }
 resource "aws_launch_configuration" "as_conf" {
-  depends_on = [aws_db_instance.default]
-  name_prefix   = "my-lc3-cli"
-  image_id      = "ami-0629230e074c580f2"
-  instance_type = t2.micro
-  security_groups    = sg-0bb5391635b3c304es
-  key_name = "jenkins"
-  iam_instance_profile = "ram-s3-role"
+  name_prefix   = var.name_prefix
+  image_id      = var.image_id
+  instance_type = var.instance_type
+  security_groups    = var.security_groups
+  user_data = "${file("userdata.sh")}"
    user_data = templatefile("${path.module}/userdata.tftpl", {endpoint = aws_db_instance.default.endpoint,password = aws_db_instance.default.password,address = aws_db_instance.default.address})
 }
-resource "aws_autoscaling_group" "bar" {
-  name                 = "my-asg3-cli" 
-  depends_on           = ["aws_launch_configuration.as_conf"]
-  launch_configuration = aws_launch_configuration.as_conf.my-lc3-cli
-  min_size             = 1
-  max_size             = 2
-  desired_capacity     = 1
-  target_group_arns   = ["aws_lb_target_group.test.us-east-2:931430496116:autoScalingGroup:7b57d37c-5d34-434a-ba6d-d0f7fd8ffa90:autoScalingGroupName/my-asg3-cli"]
- availability_zones = us-east-2b
-}
-
 resource "aws_autoscaling_group" "bar" {
   name                 = var.name 
   depends_on           = ["aws_launch_configuration.as_conf"]
@@ -46,4 +33,21 @@ resource "aws_autoscaling_group" "bar" {
   target_group_arns   = [var.target_group_arns]
  availability_zones = var.availability_zones
 }
+
+resource "aws_launch_configuration" "as_conf" {
+  depends_on = [aws_db_instance.default]
+  name_prefix   = "my-lc3-cli"
+  image_id      = "ami-0629230e074c580f2"
+  instance_type = t2.micro
+  security_groups    = sg-0bb5391635b3c304es
+  key_name = "jenkins"
+  iam_instance_profile = "ram-s3-role"
+
+
+
+
+
+
+
+
 }
